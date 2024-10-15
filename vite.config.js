f84403abc8da993ac8eff12ca60432b63f5a5cd4
Flaviant3 <flaviant3@gmail.com>
2024-10-15 16:34:33 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import legacy from '@vitejs/plugin-legacy'
+import legacy from '@vitejs/plugin-legacy';
 import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://symfony.mmi-troyes.fr:8319',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [
     vue2(),
     legacy({
-      targets: ['ie >= 11'],
+      targets: ['defaults', 'not IE 11'],
       additionalLegacyPolyfills: ['regenerator-runtime/runtime']
     })
   ],
+  esbuild: {
+    target: 'esnext',  // ou 'es2020' si vous avez besoin d'une compatibilité plus large
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
